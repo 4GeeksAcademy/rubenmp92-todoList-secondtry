04f809d2c640a48b1bcc0cc8167456d7027f8950
rubenmp92 <97123438+rubenmp92@users.noreply.github.com>
2024-02-24 19:16:05 +0000

    proyecto terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+  
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	// Add into array-> concat
+	// Delete into array-> filter
+	// Update into array-> map
+
+	const inputKeyPress = (event) => {
+		if (event.key === "Enter") {
+			setTodos([...todos,inputValue])
+			setInputValue("")
+		}
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My todos </h1>
+			<ul>
+				<li>
+				<input
+				type="text"
+				placeholder="What do you need to do?"
+				value={inputValue}
+				onChange={(e)=> setInputValue(e.target.value)}
+				onKeyDown={inputKeyPress}
+				/>   
+				</li>
+				{todos.map((item, index) => (
+  				<li key={index}
+					style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center',}}>
+   				{item}
+				<button
+					className="btn btn-light"
+					onClick={() => setTodos(todos.filter((t, currentIndex) => index !== currentIndex))}>X</button>
+				</li>
+				))}
+			</ul>
+
+<div>{todos.length} item left</div>
 		</div>
 	);
 };
